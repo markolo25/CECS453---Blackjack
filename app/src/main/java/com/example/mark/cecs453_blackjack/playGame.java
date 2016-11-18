aea7ad8e47e75cc47458f398c8c2a19778bb1bb6
KelvinPham <not2bcold@yahoo.com>
2016-11-18 10:46:10 -0800

    gameLayout

---
+++ app/src/main/java/com/example/mark/cecs453_blackjack/playGame.java
@@ -0,0 +1,13 @@
+package com.example.mark.cecs453_blackjack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class playGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_game);
+    }
+}
