3d52601df7b7a69487d23cbb729d6a3c5178a885
KelvinPham <not2bcold@yahoo.com>
2016-11-18 18:42:54 -0800

    card class

---
+++ app/src/main/java/com/example/mark/cecs453_blackjack/Card.java
@@ -0,0 +1,34 @@
+package com.example.mark.cecs453_blackjack;
+
+/**
+ * Created by Kelvin on 11/18/2016.
+ */
+
+public class Card {
+    private String name;
+    private int value;
+    private String suit;
+    public Card(String n,int v, String s){
+        name = n;
+        value = v;
+        suit = s;
+    }
+    public void setName(String n){
+        name = n;
+    }
+    public void setValue(int v){
+        value = v;
+    }
+    public void setSuit(String s){
+        suit = s;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getValue(){
+        return value;
+    }
+    public String getSuit(){
+        return suit;
+    }
+}
