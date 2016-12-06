8dffc61f6717c9ff2e55fcbde349752b8837e2e7
KelvinPham <not2bcold@yahoo.com>
2016-12-06 04:25:33 -0800

    Initialized Cards and Hands

--- app/src/main/java/com/example/mark/cecs453_blackjack/Card.java
+++ app/src/main/java/com/example/mark/cecs453_blackjack/Card.java
@@ -8,27 +8,39 @@ public class Card {
     private String name;
     private int value;
     private String suit;
-    public Card(String n,int v, String s){
+    private String image;
+    public Card(String n, int v, String s,String i){
         name = n;
         value = v;
         suit = s;
+        image = i;
     }
+
     public void setName(String n){
         name = n;
     }
+
     public void setValue(int v){
         value = v;
     }
     public void setSuit(String s){
         suit = s;
     }
+
     public String getName(){
         return name;
     }
+
     public int getValue(){
         return value;
     }
     public String getSuit(){
         return suit;
+    }
+    public void setImage(String i){
+        image = i;
+    }
+    public String getImage(){
+        return image;
     }
 }
--- app/src/main/java/com/example/mark/cecs453_blackjack/playGame.java
+++ app/src/main/java/com/example/mark/cecs453_blackjack/playGame.java
@@ -2,12 +2,102 @@ package com.example.mark.cecs453_blackjack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class playGame extends AppCompatActivity {
-
+ArrayList<Card> playingDeck= new ArrayList<Card>();
+    ArrayList<Card> opponentsHand = new ArrayList<Card>();
+    ArrayList<Card> playerHand = new ArrayList<Card>();
+int playerValue;
+    int numberOfCards = 51;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_game);
+        Button hit = (Button) findViewById(R.id.hit_button);
+        Button stay = (Button) findViewById(R.id.hit_button);
+        makeDeck();
+        deal();
     }
+    public void makeDeck(){
+        playingDeck.add(new Card("ace",11,"spades","ace_of_spades.png"));
+        playingDeck.add(new Card("ace",11,"clubs","ace_of_clubs.png"));
+        playingDeck.add(new Card("ace",11,"diamonds","ace_of_diamonds.png"));
+        playingDeck.add(new Card("ace",11,"hearts","ace_of_hearts.png"));
+
+        playingDeck.add(new Card("two",2,"spades","2_of_spades.png"));
+        playingDeck.add(new Card("two",2,"clubs","2_of_clubs.png"));
+        playingDeck.add(new Card("two",2,"diamonds","2_of_diamonds.png"));
+        playingDeck.add(new Card("two",2,"hearts","2_of_hearts.png"));
+
+        playingDeck.add(new Card("three",3,"spades","3_of_spades.png"));
+        playingDeck.add(new Card("three",3,"clubs","3_of_clubs.png"));
+        playingDeck.add(new Card("three",3,"diamonds","3_of_diamonds.png"));
+        playingDeck.add(new Card("three",3,"hearts","3_of_hearts.png"));
+
+        playingDeck.add(new Card("four",4,"spades","4_of_spades.png"));
+        playingDeck.add(new Card("four",4,"clubs","4_of_clubs.png"));
+        playingDeck.add(new Card("four",4,"diamonds","4_of_diamonds.png"));
+        playingDeck.add(new Card("four",4,"hearts","4_of_hearts.png"));
+
+        playingDeck.add(new Card("five",5,"spades","5_of_spades.png"));
+        playingDeck.add(new Card("five",5,"clubs","5_of_clubs.png"));
+        playingDeck.add(new Card("five",5,"diamonds","5_of_diamonds.png"));
+        playingDeck.add(new Card("five",5,"hearts","5_of_hearts.png"));
+
+        playingDeck.add(new Card("six",6,"spades","6_of_spades.png"));
+        playingDeck.add(new Card("six",6,"clubs","6_of_clubs.png"));
+        playingDeck.add(new Card("six",6,"diamonds","6_of_diamonds.png"));
+        playingDeck.add(new Card("six",6,"hearts","6_of_hearts.png"));
+
+        playingDeck.add(new Card("seven",7,"spades","7_of_spades.png"));
+        playingDeck.add(new Card("seven",7,"clubs","7_of_spades.png"));
+        playingDeck.add(new Card("seven",7,"diamonds","7_of_spades.png"));
+        playingDeck.add(new Card("seven",7,"hearts","7_of_spades.png"));
+
+        playingDeck.add(new Card("eight",8,"spades","8_of_spades.png"));
+        playingDeck.add(new Card("eight",8,"clubs","8_of_clubs.png"));
+        playingDeck.add(new Card("eight",8,"diamonds","8_of_diamonds.png"));
+        playingDeck.add(new Card("eight",8,"hearts","8_of_hearts.png"));
+
+        playingDeck.add(new Card("nine",9,"spades","9_of_spades.png"));
+        playingDeck.add(new Card("nine",9,"clubs","9_of_clubs.png"));
+        playingDeck.add(new Card("nine",9,"diamonds","9_of_diamonds.png"));
+        playingDeck.add(new Card("nine",9,"hearts","9_of_hearts.png"));
+
+        playingDeck.add(new Card("jack",10,"spades","jack_of_spades.png"));
+        playingDeck.add(new Card("jack",10,"clubs","jack_of_clubs.png"));
+        playingDeck.add(new Card("jack",10,"diamonds","jack_of_diamonds.png"));
+        playingDeck.add(new Card("jack",10,"hearts","jack_of_hearts.png"));
+
+        playingDeck.add(new Card("queen",10,"spades","queen_of_spades.png"));
+        playingDeck.add(new Card("queen",10,"clubs","queen_of_clubs.png"));
+        playingDeck.add(new Card("queen",10,"diamonds","queen_of_diamonds.png"));
+        playingDeck.add(new Card("queen",10,"hearts","queen_of_hearts.png"));
+
+        playingDeck.add(new Card("king",10,"spades","king_of_spades.png"));
+        playingDeck.add(new Card("king",10,"clubs","king_of_clubs.png"));
+        playingDeck.add(new Card("king",10,"diamonds","king_of_diamonds.png"));
+        playingDeck.add(new Card("king",10,"hearts","ace_of_hearts.png"));
+    }
+    public void deal(){
+        Random randomNumber = new Random();
+        int randomCard;
+        for(int i = 1; i < 2; i++){
+            randomCard = randomNumber.nextInt(numberOfCards);
+            playerHand.add(playingDeck.get(randomCard));
+            playingDeck.remove(playingDeck.get(randomCard));
+            numberOfCards--;
+        }
+        for(int i = 1; i < 2; i++){
+            randomCard = randomNumber.nextInt(numberOfCards);
+            opponentsHand.add(playingDeck.get(randomCard));
+            playingDeck.remove(playingDeck.get(randomCard));
+            numberOfCards--;
+        }
+    }
+
 }
